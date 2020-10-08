1291aae65345e5e446696dc6467476481dc59bdc
ylu <ylu@ert.com>
2020-10-08 12:11:59 -0400

    use db url string

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 const Pool = require('pg').Pool;
 
-const dbURLForDev = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-// postgresql://YYYYYY:XXXXX@localhost:5432/ZZZZZZ
-
-const dbURLForProd = process.env.DATABASE_URL; //heroku addons
-
-const connectionString = process.env.NODE_ENV === 'production' ? dbURLForProd : dbURLForDev;
+const connectionString = process.env.DATABASE_URL; //heroku addons
 console.log(connectionString);
 
-const pool = new Pool({connectionString});
+const pool = new Pool({ connectionString });
 
 module.exports = pool;
